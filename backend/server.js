1874831f3a3bdf584f0cbc41f8edacd2a911fe64
returndaniels <daniel.lipe@poli.ufrj.br>
2021-02-18 01:58:44 -0300

    Adiciinada a rota principal

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ require('./database');
 
 const port = process.env.PORT || 4200;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -19,38 +21,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
+app.use(router);
 
-io.on('connect', (socket) => {
-    socket.on('join', ({ name, islogged }, callback) => {
-        const { error, user } = createUser({ _id: socket.id, name, islogged });
+io.on('connection', (socket) => {
+  socket.on('join', ({ name, islogged }, callback) => {
+      const { error, user } = createUser({ _id: socket.id, name, islogged });
 
-        if(error) return callback(error);
+      if(error) return callback(error);
 
-        socket.emit('joined', { _id: socket.id, name, islogged });
-        socket.broadcast.emit('joinedEvent', {  
-            text: `${name} entrou no chat` 
-        });
-  
-        callback();
-    });
-  
-    socket.on('sendMessage', (message, callback) => {
-      const user = getUser(socket.id);
-  
-      io.emit('message', { user: user.name, text: message, date: new Date() });
-  
+      socket.emit('joined', { _id: socket.id, name, islogged });
+      socket.broadcast.emit('joinedEvent', {  
+          text: `${name} entrou no chat` 
+      });
+
       callback();
-    });
-  
-    socket.on('disconnect', (user, callback) => {
-      const { error } = deleteUser(socket.id);
-      
-      callback(error);
-  
-      if(user) {
-        io.emit('joinedEvent', { text: `${user.name} saiu do chat` });
-      }
-    })
   });
 
-server.listen(4200, () => console.log(`Server running on ${port}`));
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.emit('message', { user: user.name, text: message, date: new Date() });
+
+    callback();
+  });
+
+  socket.on('disconnect', (user, callback) => {
+    const { error } = deleteUser(socket.id);
+    if(callback) {
+      callback(error);
+    }
+    
+    if(user) {
+      io.emit('joinedEvent', { text: `${user.name} saiu do chat` });
+    }
+  })
+});
+
+server.listen(port, () => console.log(`Server has started.`));
